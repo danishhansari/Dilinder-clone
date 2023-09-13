7168b796c95d8f04b7f3340a9159769b21f43b6e
Danish <dan71ish@gmail.com>
2023-09-13 09:39:31 +0530

    some styling

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 const textArea = document.querySelector('.text-input textarea');
 const preview = document.querySelector('.preview');
+const words = document.querySelector('.words')
+const character = document.querySelector('.char')
 
 window.addEventListener('load', ()  => {
-    console.log(textArea.value.split(' '));
-    let high = textArea.value.split(' ');
-    high.forEach(element => {
-        hljs.highlightElement(element)
-    });
-})
-function displayPreview () {
-    preview.innerHTML = marked.parse(textArea.value);
-}
+    // let high = textArea.value.split(' ');
+    // high.forEach(element => {
+        //     hljs.highlightElement(element)
+        // });
+    })
+    function displayPreview () {
+            preview.innerHTML = marked.parse(textArea.value);
+            let characterVal = textArea.value.split(' ');
+            let wordsVal = textArea.value.split('');
+            console.log(words.length/400);
+            words.textContent = wordsVal.length;
+            character.textContent = characterVal.length;
+        }
 
-textArea.addEventListener('keyup', displayPreview);
+textArea.addEventListener('input', displayPreview);
